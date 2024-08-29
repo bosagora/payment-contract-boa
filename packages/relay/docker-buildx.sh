b9883fb59ca722a9973126e8c0bb763c27dc17ff
MichaelKim20 <michael.kim.200518@gmail.com>
2024-08-29 09:23:37 +0900

    Change image tag generation rules when creating docker images

--- packages/relay/docker-buildx.sh
+++ packages/relay/docker-buildx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAG_NANE="$(git rev-parse --abbrev-ref HEAD)-$(git rev-parse --short=6 HEAD)"
+TAG_NANE="$(git rev-parse --short=12 HEAD)"
 echo "TAG_NANE=$TAG_NANE"
 
 docker buildx build --platform=linux/amd64,linux/arm64 -t bosagora/acc-relay:"$TAG_NANE" -f Dockerfile --push .
